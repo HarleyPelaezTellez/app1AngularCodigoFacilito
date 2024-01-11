7c24ca8574ea4eb7bf565c32753b562a12da0903
HarleyPelaezTellez <arleypelaez1998@outlook.com>
2024-01-10 23:29:20 -0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AtributoComponent } from './components/atributo/atributo.component';
 import { EstructuralComponent } from './components/estructural/estructural.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AtributoComponent, EstructuralComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AtributoComponent, EstructuralComponent, PipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
